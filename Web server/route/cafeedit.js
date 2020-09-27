6f6cf671daefb3c8dd6d3e81542ff5c7b9cd2362
HanJungWoo <hjw9504@ajou.ac.kr>
2020-09-27 20:16:11 +0900

    web server update

--- Web server/route/cafeedit.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var dbQuery = require("../database/promiseQuery.js");
-//카페 정보수정 페이지(카페사장전용)
-
-module.exports = router;
--- Web server/route/education.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var dbQuery = require("../database/promiseQuery.js");
-
-module.exports = router;
